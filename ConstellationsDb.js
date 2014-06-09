4855389f82739e87b0d298ef65b611820abd8e9c
dl <d@1.com>
2014-06-10 00:45:14 +0300

    Created ConstellationsDb with Taurus definition.

---
+++ ConstellationsDb.js
@@ -0,0 +1,35 @@
+	var constellation = [];
+		
+	// Taurus
+	constellation['Taurus'] = {
+	   leftBottomCornerX:  0,
+	   leftBottomCornerY:  6,
+	   rightTopCornerX:    30,
+	   rightTopCornerY:    3,
+	   infoHeading: 'Taurus Constellation',
+	   infoContent: 'Taurus is one of the constellations of the zodiac,'+
+			'which means it is crossed by the plane of the ecliptic. Its name is a Latin word meaning "bull", and'+
+			'its astrological symbol is a stylized bull\'s head: Taurus.svg (Unicode ♉). Taurus is a large and prominent'+
+			'constellation in the northern hemisphere\'s winter sky. It is one of the oldest constellations, dating back to'+
+			'at least the Early Bronze Age when it marked the location of the Sun during the spring equinox. Taurus came to'+
+			'symbolize the bull in the mythologies of Ancient Babylon, Egypt and Greece.</p>',
+	   infoWikiLink: 'http://en.wikipedia.org/wiki/Taurus_(constellation)'
+	   }
+	  
+	  
+	  
+	  //Add a Ground Layer Cancer
+	constellation['Cancer'] = {
+	   leftBottomCornerX:  0,
+	   leftBottomCornerY:  10,
+	   rightTopCornerX:   40,
+	   rightTopCornerY:    7,
+	   infoHeading: 'Cancer Constellation',
+	   infoContent: 'Cancer is one of the constellations of the zodiac,'+
+			'which means it is crossed by the plane of the ecliptic. Its name is a Latin word meaning "bull", and'+
+			'its astrological symbol is a stylized bull\'s head: Cancer.svg (Unicode ♉). Cancer is a large and prominent'+
+			'constellation in the northern hemisphere\'s winter sky. It is one of the oldest constellations, dating back to'+
+			'at least the Early Bronze Age when it marked the location of the Sun during the spring equinox. Cancer came to'+
+			'symbolize the bull in the mythologies of Ancient Babylon, Egypt and Greece.</p>',
+	   infoWikiLink: 'http://en.wikipedia.org/wiki/Cancer_(constellation)'
+	   }
